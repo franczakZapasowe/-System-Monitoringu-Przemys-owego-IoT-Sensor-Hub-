41301269c20c114dcdee14ddbe4ac9ec7ce5bdd3
franczakZapasowe <russeldurantmvp@gmail.com>
2026-08-25 01:47:51 +0200

    Dodanie wielowatkowsci

--- server.cpp
+++ server.cpp
@@ -4,7 +4,24 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "Structures.h"
+#include <thread>
 
+void handleClient(int clientSocket) {
+    MachineState machineState{};
+    ServerCommand comand{};
+    comand.m_code=true;
+    while (true) {
+        int recive = recv(clientSocket, &machineState, sizeof(MachineState), 0);
+        if (recive > 0 ) std::cout<<"Id: "<<machineState.m_id<<" temeprature: "<<machineState.m_temperature<<std::endl;
+        else if (recive <= 0) break;
+        if (machineState.m_temperature > 95) {
+            comand.m_code=false;
+            std::cout<<"Alarm\n";
+        }
+        send(clientSocket, &comand, sizeof(ServerCommand), 0);
+    }
+    close(clientSocket);
+}
 
 int main() {
 
@@ -33,25 +50,15 @@ int main() {
 
     sockaddr_in clientAddr{};
     socklen_t clientAddrLen = sizeof(clientAddr);
-    int clientSocket = accept(serverSocket, reinterpret_cast<sockaddr *>(&clientAddr), &clientAddrLen);
-    if (clientSocket == -1) {
-        std::cerr<<"accept failed"<<std::endl;
-        return 1;
-    }
-
-    MachineState machineState{};
-    ServerCommand comand{};
-    comand.m_code=true;
     while (true) {
-        int recive = recv(clientSocket, &machineState, sizeof(MachineState), 0);
-        if (recive > 0 ) std::cout<<"Id: "<<machineState.m_id<<" temeprature: "<<machineState.m_temperature<<std::endl;
-        else if (recive <= 0) break;
-        if (machineState.m_temperature > 95) {
-            comand.m_code=false;
-            std::cout<<"Alarm\n";
+        int clientSocket = accept(serverSocket, reinterpret_cast<sockaddr *>(&clientAddr), &clientAddrLen);
+        if (clientSocket == -1) {
+            std::cerr<<"accept failed"<<std::endl;
+            return 1;
         }
-        send(clientSocket, &comand, sizeof(ServerCommand), 0);
+
+        std::thread t (handleClient, clientSocket);
+        t.detach();
+        std::cout<<"NumerWatku: "<<t.get_id()<<std::endl;
     }
-    close(clientSocket);
-    close(serverSocket);
 }
